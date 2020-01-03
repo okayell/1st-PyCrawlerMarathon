7cdb8e5fc7b27e79273c97c4f226cca2e89c16bd
Jeff Huang <okayell@hotmail.com>
2020-01-03 23:22:20 +0800

    hw_29

---
+++ homework/Day029_HW/Day029_Scrapy_PTT/myproject/main.py
@@ -0,0 +1,16 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+def main():
+    # target_board = ['Gossiping', 'Stock']
+    target_board = ['Gossiping']
+    process = CrawlerProcess(get_project_settings())
+    for board in target_board:
+        process.crawl('PTTCrawler', board=board)
+        process.start()
+
+
+if __name__ == '__main__':
+    main()
